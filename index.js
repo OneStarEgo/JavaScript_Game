48795b5a5890eef6b71e892ea82f9a8bd641ff1c
Rodney McGee <rodneymcgee73@gmail.com>
2023-05-22 14:19:33 -0500

    Create styles.css + Create Canvas

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Hello World')
+const canvas = document.querySelector('canvas');
+
+const c = canvas.getContext('2d')
+
+canvas.width = 1024
+canvas.height = 576
+
+c.fillStyle = 'white'
+c.fillRect(0, 0, canvas.width, canvas.height)
