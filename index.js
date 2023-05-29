b364a2a2e56defa1d53a6741434dd02cfb6e3853
Rodney McGee <rodneymcgee73@gmail.com>
2023-05-29 11:07:52 -0500

    Add Event Listener for Pressed Keys + Declare animate Function + Define Sprite class

--- index.js
+++ index.js
@@ -14,8 +14,27 @@ playerImage.src = './img/playerDown.png'
 const image = new Image()
 image.src = './img/Pokemon Clone Map.png'
 
-image.onload = () => {
-    c.drawImage(image, -515, -1000)
+class Sprite {
+    constructor({ position, velocity, image }) {
+        this.position = position
+        this.image = image
+    }
+    draw() {
+        c.drawImage(this.image, -515, -1000)
+
+    }
+}
+
+const background = new Sprite({
+    position: {
+    x: -515,
+    y: -1000
+    },
+    image: image
+})
+
+function animate() {
+    window.requestAnimationFrame(animate)
     c.drawImage(
       playerImage,
       0,
@@ -28,3 +47,24 @@ image.onload = () => {
       playerImage.height
     )
 }
+animate()
+
+
+window.addEventListener('keydown', (e) => {
+    console.log(e.key)
+    switch (e.key) {
+        case 'w':
+            console.log('pressed w key')
+            break
+        case 'a':
+            console.log('pressed a key')
+            break
+        case 's':
+            console.log('pressed s key')
+            break
+        case 'd':
+            console.log('pressed d key')
+            break
+
+    }
+})
