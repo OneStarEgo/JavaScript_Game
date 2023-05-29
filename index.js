1a14bf2adb1cfc8cd2abcec81ac8aaaa853087cf
Rodney McGee <rodneymcgee73@gmail.com>
2023-05-29 10:10:52 -0500

    Redesign Pokemon Clone Map + Add Player Sprites to Img Folder + Render Player Sprite on Map

--- index.js
+++ index.js
@@ -7,3 +7,24 @@ canvas.height = 576
 
 c.fillStyle = 'white'
 c.fillRect(0, 0, canvas.width, canvas.height)
+
+const playerImage = new Image()
+playerImage.src = './img/playerDown.png'
+
+const image = new Image()
+image.src = './img/Pokemon Clone Map.png'
+
+image.onload = () => {
+    c.drawImage(image, -515, -1000)
+    c.drawImage(
+      playerImage,
+      0,
+      0,
+      playerImage.width / 4,
+      playerImage.height,
+      canvas.width / 2 - playerImage.width / 4 / 2,
+      canvas.height / 2 - playerImage.height / 2,
+      playerImage.width / 4,
+      playerImage.height
+    )
+}
